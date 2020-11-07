ab6ece0a97320d2179193636418ae983c2d3c2a3
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2020-11-07 19:37:38 +0900

    env?

--- src/index.ts
+++ src/index.ts
@@ -354,6 +354,7 @@ async function getKibelaNoteUnfurlFromUrl(url: string): Promise<[string, Message
 }
 
 app.event('link_shared', async({event, client}) => {
+  console.log(process.env)
   const channel = event.channel;
   const messageTs = event.message_ts;
   Promise.all(event.links.map(async (link) => getKibelaNoteUnfurlFromUrl(link.url as string))).then(values => {
