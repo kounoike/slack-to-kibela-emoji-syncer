e730e1313302a968df747be99cfd7b0080be7be8
KOUNOIKE Yuusuke <yuusuke.kounoike@stadium.co.jp>
2020-11-09 02:28:52 +0900

    root route

--- src/index.ts
+++ src/index.ts
@@ -22,8 +22,8 @@ const { JSDOM } = require('jsdom')
 const Canvas = require('canvas')
 const fabric = require('fabric').fabric
 
-// const dicPath = './node_modules/kuromoji/dict';
-const dicPath = './dict';
+const dicPath = './node_modules/kuromoji/dict';
+// const dicPath = './dict';
 // const targetPosList = ['名詞', '形容詞', '動詞'];
 const targetPosList = ['名詞'];
 const ngWords = ['https', '://', '[', ']', '@', 'co', 'jp', 'com', '/', 'in', "もの","これ","ため","それ","ところ","よう", "の", "こと", "とき", "ん"]
@@ -513,6 +513,9 @@ receiver.app.post('/kibela-webhook', (req, res) => {
       console.error(json);
     }
   })
+});
+receiver.app.get('/', (req, res) => {
+  res.send('OK');
 });
 
 (async () => {
